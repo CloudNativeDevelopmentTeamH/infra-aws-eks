c3d483f8f4faa4a389b045754598f0387b85b22c
David Linhardt <divad2012@web.de>
2025-12-29 16:00:25 +0100

    add vars for argo

--- variables.tf
+++ variables.tf
@@ -1,3 +1,4 @@
+# AWS
 variable "aws_region" {
   default = "eu-central-1"
 }
@@ -5,4 +6,37 @@ variable "aws_region" {
 variable "cluster_name" {
   default = "cnd-prod-eks"
   type    = string
+}
+
+# ArgoCD
+variable "argo_repo_url" {
+  default = "https://github.com/CloudNativeDevelopmentTeamH/infra-k8s.git"
+  type    = string
+}
+
+variable "argo_repo_revision" {
+  default = "main"
+  type    = string
+}
+
+variable "argo_root_path" {
+  description = "app-of-apps folder"
+  type        = string
+}
+
+variable "argo_chart_version" {
+  description = "ArgoCD Helm Chart Version"
+  default     = ""
+  type        = string
+}
+
+variable "argo_image_updater_chart_version" {
+  description = "ArgoCD Image Updater Helm Chart Version"
+  default = ""
+  type        = string
+}
+
+variable "install_image_updater" {
+  default = true
+  type    = bool
 }
