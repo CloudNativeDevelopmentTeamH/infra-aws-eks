2e6264bec118ae01ad67a27540cbcf7188f5a532
David Linhardt <divad2012@web.de>
2025-12-29 16:00:15 +0100

    add namespaces for argo

---
+++ argo.tf
@@ -0,0 +1,9 @@
+# Namespaces
+resource "kubernetes_namespace" "argocd" {
+  metadata { name = "argocd" }
+}
+
+resource "kubernetes_namespace" "argoci" {
+  count = var.install_image_updater ? 1 : 0
+  metadata { name = "argocd-image-updater" }
+}
