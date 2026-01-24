fccb4ea4bfc6770802790d49f426047f389cc066
Steven Burger <46164156+cwsyth@users.noreply.github.com>
2026-01-24 17:02:49 +0100

    fix wrong service account for alb assume role policy

--- alb-controller.tf
+++ alb-controller.tf
@@ -267,7 +267,7 @@ data "aws_iam_policy_document" "alb_controller_assume_role" {
     condition {
       test     = "StringEquals"
       variable = "${replace(aws_iam_openid_connect_provider.eks.url, "https://", "")}:sub"
-      values   = ["system:serviceaccount:auth:eks-service-account"]
+      values   = ["system:serviceaccount:kube-system:aws-load-balancer-controller"]
     }
   }
 }
